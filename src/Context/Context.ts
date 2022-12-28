a9353aed24847f33f10db8d56949df9c879bbe80
oibioib <pavelbodal@gmail.com>
2022-12-28 21:31:45 +0300

    feat: add context

--- src/Context/Context.ts
+++ src/Context/Context.ts
@@ -1,4 +1,7 @@
 import { createContext } from 'react';
+import { FilterValue } from '../types/FilterTypes';
 import { Product } from '../types/ProductTypes';
 
 export const productsContext = createContext<Product[]>([]);
+export const brandsContext = createContext<FilterValue[]>([]);
+export const categoriesContext = createContext<FilterValue[]>([]);
