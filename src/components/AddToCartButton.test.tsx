1138b6980c8338448f62c27c6a5b800cbbd7c8e0
Aleksandr <a.teslin90@gmail.com>
2023-01-04 12:18:57 +0300

    feat: add 10 tests in 3 suits

--- src/components/AddToCartButton.test.tsx
+++ src/components/AddToCartButton.test.tsx
@@ -2,19 +2,18 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import AddToCartButton from './AddToCartButton';
 
-describe('Register component', () => {
+describe('AddToCartComponent component', () => {
+  const key = 'OA_cart';
+  ////////////////Hard cord id to check from 1 to 25
+  const id = 1;
+
   test('renders button and the text', () => {
-    render(<AddToCartButton id={1} />);
+    render(<AddToCartButton id={id} />);
     const linkElement = screen.getByText(/Add to cart/i);
-    const button = screen.getByRole('button');
     expect(linkElement).toBeInTheDocument();
-    expect(button).toBeInTheDocument();
   });
 
   test('should add to localStorage on click', () => {
-    const key = 'OA_cart';
-    ////////////////Hard cord id to check from 1 to 25
-    const id = 1;
     render(<AddToCartButton id={id} />);
     const buttonElement = screen.getByText('Add to cart');
     userEvent.click(buttonElement);
@@ -23,11 +22,17 @@ describe('Register component', () => {
   });
 
   test('should change the button from add to cart => drop if item is in localStorage', () => {
-    const id = 1;
-    const key = 'OA_cart';
     localStorage.setItem(key, JSON.stringify({ [id]: 1 }));
     render(<AddToCartButton id={id} />);
     const dropButton = screen.getByText('Drop');
     expect(dropButton).toBeInTheDocument();
+  });
+
+  test('"Add to cart" should be removed from the document onClick', () => {
+    render(<AddToCartButton id={id} />);
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+    const isButtonElement = screen.queryByText('Add to cart', { exact: false });
+    expect(isButtonElement).toBeNull;
   });
 });
