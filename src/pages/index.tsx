19f30ad4ad936f7ca4d08b4901dfbd56e7abb1d1
oibioib <103119318+oibioib@users.noreply.github.com>
2023-01-10 00:04:25 +0300

    Revert "Fix search case"

--- src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import MainPage from './MainPage';
-import CartPage from './CartPage';
-import ProductPage from './ProductPage';
-import ErrorPage from './ErrorPage';
-
-export { MainPage, CartPage, ProductPage, ErrorPage };
